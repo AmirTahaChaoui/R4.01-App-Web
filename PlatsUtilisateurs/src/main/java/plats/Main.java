5138a6ed6934d2cbdb9a89aaf2556fb2316e4e89
AmirTahaChaoui <amir.TAHA-CHAOUI@etu.univ-amu.fr>
2026-04-08 15:21:13 +0200

    Amélioration du Main et du pom.xml

--- PlatsUtilisateurs/src/main/java/plats/Main.java
+++ PlatsUtilisateurs/src/main/java/plats/Main.java
@@ -7,19 +7,24 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.net.URI;
 
 public class Main {
+    // URI de base sur laquelle le serveur Grizzly va écouter
     public static final String BASE_URI = "http://localhost:8080/PlatsUtilisateurs/api/";
 
     public static HttpServer startServer() {
+        // Scanner le package des ressources pour les enregistrer
         final ResourceConfig rc = new ResourceConfig().packages("plats.resources");
+        
+        // Créer et démarrer une nouvelle instance du serveur Grizzly
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
     public static void main(String[] args) throws Exception {
         final HttpServer server = startServer();
         System.out.println(String.format(
-            "Application disponible sur %sapi/plats et %sapi/utilisateurs\nAppuyez sur Entrée pour arrêter...",
+            "Application lancée sur %splats et %sutilisateurs\nAppuyez sur Entrée pour arrêter le serveur...",
             BASE_URI, BASE_URI));
+            
         System.in.read();
-        server.shutdown();
+        server.shutdownNow();
     }
 }
